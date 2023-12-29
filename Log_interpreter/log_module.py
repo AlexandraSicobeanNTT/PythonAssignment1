4fe0bda809de76d98fe945590071be9cc6e98d5a
Bogdan Draghici <dbogdangabriel@gmail.com>
2023-12-29 15:37:41 +0200

    modified ex4,5,6 and write separate method to call our methods

--- Log_interpreter/log_module.py
+++ Log_interpreter/log_module.py
@@ -14,7 +14,7 @@ def parse_log_entry(line):
     else:
         return None
 
-
+# read file function
 def read_file(file_path):
     logs = []
     with open(file_path, 'r') as file:
@@ -25,7 +25,7 @@ def read_file(file_path):
     return logs
 
 #ex 4
-def find_app_with_most_errors(logs):
+def find_app_with_most_errors(file_path):
     app_error_count = {
         "BackendApp": 0,
         "FrontendApp": 0,
@@ -33,31 +33,25 @@ def find_app_with_most_errors(logs):
         "System": 0
     }
 
-    with open(logs, 'r') as file:
-        content = file.readlines()
+    logs = read_file(file_path)
 
-    for line in content:
-        if "[ERROR]" in line:
-            if "BackendApp" in line:
-                app_error_count["BackendApp"] += 1
-            elif "FrontendApp" in line:
-                app_error_count["FrontendApp"] += 1
-            elif "API" in line:
-                app_error_count["API"] += 1
-            elif "SYSTEM" in line:
-                app_error_count["System"] += 1
+    for log_entry in logs:
+        if log_entry['log-type'] == 'ERROR':
+            app = log_entry['app-type']
+            if app in app_error_count:
+                app_error_count[app] += 1
 
     if app_error_count:
         most_errors_app = max(app_error_count, key=app_error_count.get)
-        count_error = app_error_count[most_errors_app]
+        error_count = app_error_count[most_errors_app]
 
-        return most_errors_app, count_error
+        return most_errors_app, error_count
     else:
         return None, 0
 
 
-#ex 5
-def find_app_with_most_successful_runs(logs):
+# ex 5
+def find_app_with_most_successful_runs(file_path):
     app_success_count = {
         "BackendApp": 0,
         "FrontendApp": 0,
@@ -65,19 +59,13 @@ def find_app_with_most_successful_runs(logs):
         "System": 0
     }
 
-    with open(logs, 'r') as file:
-        content = file.readlines()
+    logs = read_file(file_path)
 
-    for line in content:
-        if "[INFO]" in line:
-            if "BackendApp" in line:
-                app_success_count["BackendApp"] += 1
-            elif "FrontendApp" in line:
-                app_success_count["FrontendApp"] += 1
-            elif "API" in line:
-                app_success_count["API"] += 1
-            elif "SYSTEM" in line:
-                app_success_count["System"] += 1
+    for log_entry in logs:
+        if log_entry['log-type'] == 'INFO':
+            app = log_entry['app-type']
+            if app in app_success_count:
+                app_success_count[app] += 1
 
     if app_success_count:
         most_success_app = max(app_success_count, key=app_success_count.get)
@@ -88,21 +76,19 @@ def find_app_with_most_successful_runs(logs):
         return None, 0
 
 
-
-#ex 6
-def find_third_of_day_with_most_failures(logs):
+# ex 6
+def find_third_of_day_with_most_failures(file_path):
     thirds = {
         "00:00:00 - 07:59:59": 0,
         "08:00:00 - 15:59:59": 0,
         "16:00:00 - 23:59:59": 0
     }
 
-    with open(logs, 'r') as file:
-        content = file.readlines()
+    logs = read_file(file_path)
 
-    for line in content:
-        if "[ERROR]" in line:
-            timestamp = line.split(" - ")[0]
+    for log_entry in logs:
+        if log_entry['log-type'] == 'ERROR':
+            timestamp = log_entry['timestamp']
             hour = int(timestamp.split(":")[0])
 
             if hour >= 0 and hour <= 7:
@@ -120,18 +106,8 @@ def find_third_of_day_with_most_failures(logs):
     else:
         return None, 0
 
-# Usage ex 4
-app, error_count = find_app_with_most_errors("../Log_info/output.txt")
-print(f"The app with the most failed runs is {app} with {error_count} errors.")
 
-# Usage ex 5
-app, success_count = find_app_with_most_successful_runs("../Log_info/output.txt")
-print(f"The app with the most successful runs is {app} with {success_count} successful runs.")
-
-# Usage ex 6
-third, failure_count = find_third_of_day_with_most_failures("../Log_info/output.txt")
-print(f"The {third} time interval had the most failed runs with {failure_count} failures.")
-
+# ex 7
 def longest_and_shortest_successful_runtime_per_app_type(logs):
     app_types = ['BackendApp', 'FrontendApp', 'API', 'SYSTEM']
     runtimes = {app_type: {'max_runtime': float('-inf'), 'min_runtime': float('inf')} for app_type in app_types}
@@ -152,6 +128,7 @@ def longest_and_shortest_successful_runtime_per_app_type(logs):
     print(runtimes)
 
 
+# ex 8
 def hour_of_the_day_with_most_activity_per_app_type(logs):
     app_types = ['BackendApp', 'FrontendApp', 'API', 'SYSTEM']
     hourly_logs = {app_type: {hour: 0 for hour in range(24)} for app_type in app_types}
@@ -169,7 +146,7 @@ def hour_of_the_day_with_most_activity_per_app_type(logs):
 
     return hours_with_most_activity
 
-
+# ex 9
 def compute_failure_rate_per_app_type(logs):
     app_types = ['BackendApp', 'FrontendApp', 'API', 'SYSTEM']
     failure_rates = []
@@ -180,3 +157,19 @@ def compute_failure_rate_per_app_type(logs):
         failure_rates.append({'app-type': app_type, 'failure_rate': error_count / total_count})
 
     return failure_rates
+
+
+# Main
+# Create a dictionary mapping method names to their corresponding functions
+methods = {
+    "App with Most Errors and how many ": find_app_with_most_errors, # ex 4
+    "App with Most Successful Runs and how many": find_app_with_most_successful_runs, # ex 5
+    "Third of Day with Most Failures": find_third_of_day_with_most_failures, # ex 6
+    # rest of the methods here
+}
+
+# Iterate over the dictionary and execute each method
+for method_name, method_func in methods.items():
+    result = method_func("../Log_info/output.txt")
+    print(f"{method_name}: {result}")
+    print()
